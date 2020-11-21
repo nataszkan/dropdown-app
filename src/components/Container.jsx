4edeac7c385a24a361f7c3f29a8f4147a2b582e9
Natasza Nowakowska <natasza.nowakowska@valtech.com>
2020-11-21 14:18:28 +0100

    Clean up

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Container extends Component {
+    render() {
+        return (
+            <div>my page</div>
+        )
+    }
+}
+
+export default Container;
