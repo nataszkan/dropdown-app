3d54ac398a524eb9143273087514c40da690dc9e
Natasza Nowakowska <natasza.nowakowska@valtech.com>
2020-11-21 15:38:09 +0100

    Add dropdown, add bacis styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Dropdown from './components/Dropdown'
 
 class App extends Component {
   render () {
     return (
       <div>
         <h1>Hello dropdown</h1>
+        <Dropdown />
       </div>
     )
   }
---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+class Dropdown extends Component {
+    render () {
+        return (
+            <div>
+                <button>...</button>
+                <ul className="Dropdown">
+                    <li>edit</li>
+                    <li>share</li>
+                    <li>delete</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Dropdown
