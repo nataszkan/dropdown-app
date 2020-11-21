366f6709a15f9fbbe4b22a6e3aea462e5dfce015
Natasza Nowakowska <natasza.nowakowska@valtech.com>
2020-11-21 22:25:16 +0100

    Refactor with buttons in one component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
-import Dropdown from './components/Dropdown'
 
 class App extends Component {
+  constructor() {
+    super();
+    
+    this.state= {
+      showMenu: false,
+    }
+    
+    this.showMenu = this.showMenu.bind(this);
+  }
+  
+  showMenu(event){
+    event.preventDefault();
+    
+    this.setState({
+      showMenu: true,
+    });
+  }
+  
   render () {
     return (
       <div>
-        <h1>Hello dropdown</h1>
-        <Dropdown />
+        <div>
+          <button onClick={this.showMenu}>show menu</button>
+        </div>
+        
+        {
+          this.state.showMenu
+          ? (
+            <ul>
+              <li><button>edit</button></li>
+              <li><button>share</button></li>
+              <li><button>delete</button></li>
+            </ul>
+          )
+          : (
+            null
+          )
+        }
       </div>
     )
   }
